8d6797b265cb2881286af1ad0b86b58168357bf7
Xavier Duthil <x.duthil@gmail.com>
2017-11-26 19:47:44 +0100

    Add current track displaying

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,9 +1,10 @@
-var currentUrl, titleElement, videoElement, nextButtonElement;
+var currentUrl, titleElement, videoElement, nextButtonElement, descriptionElement, trackList;
 
 function getElements() {
   titleElement = document.querySelectorAll("h1.title")[0];
   videoElement = document.getElementsByTagName("video")[0];
   nextButtonElement = document.getElementsByClassName("ytp-next-button")[0];
+  descriptionElement = document.getElementById("description");
 }
 
 function getCurrentVideo() {
@@ -41,8 +42,69 @@ function nextVideo() {
   nextButtonElement.click();
 }
 
+function getCurrentTime() {
+  if (!videoElement) {
+    getElements();
+  }
+  return videoElement.currentTime;
+}
+
+function getCurrentTrack() {
+  if (!videoElement) {
+    getElements();
+  }
+  if (!trackList || Object.keys(trackList).length === 0) {
+    getElements();
+    trackList = buildTrackList();
+  }
+
+  var currentTime = getCurrentTime();
+  var trackTitle = "";
+  for (var trackStartTime in trackList) {
+    if (trackStartTime > currentTime) {
+      break;
+    }
+    trackTitle = trackList[trackStartTime];
+  }
+  return trackTitle;
+}
+
+function buildTrackList() {
+  if (!descriptionElement) {
+    getElements();
+  }
+  var trackList = {};
+  var descriptionStr = descriptionElement.textContent;
+  var descriptionLines = descriptionStr.split("\n");
+  var timestampRegex = /(\d+:)?(\d\d):(\d\d)/;
+
+  for (var lineNum in descriptionLines) {
+    var line = descriptionLines[lineNum];
+    var regexResult = line.match(timestampRegex);
+    if (!regexResult) {
+      continue
+    }
+
+    var timestamp = regexResult[0];
+    line = line.replace(timestamp, "").trim();
+
+    var time = parseTime(regexResult[1], regexResult[2], regexResult[3]);
+    trackList[time] = line;
+  }
+
+  return trackList;
+}
+
+function parseTime(hours, minutes, seconds) {
+  hours = parseInt(hours) || 0;
+  minutes = parseInt(minutes) || 0;
+  seconds = parseInt(seconds) || 0;
+  return hours * 3600 + minutes * 60 + seconds;
+}
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (!currentUrl || currentUrl !== location.href) {
+    trackList = {};
     getElements();
     currentUrl = location.href;
   }
@@ -52,7 +114,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
       sendResponse(getCurrentVideo());
       break;
     case "getCurrentTrack":
-      sendResponse(getCurrentVideo());
+      sendResponse(getCurrentTrack());
       break;
     case "playOrPause":
       playOrPause();
