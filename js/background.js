815aa64ab7dbbd53dca06af9a2db744c6f262d2e
Dmitry Maganov <vonagam@gmail.com>
2019-01-06 14:42:59 +0300

    New functionality: Click on progress bar to go to position in track

--- js/background.js
+++ js/background.js
@@ -65,7 +65,10 @@ function fastForward() {
   chrome.tabs.sendMessage(trackedTabId, "fastForward");
 }
 function goToTrack(trackIdx) {
-  chrome.tabs.sendMessage(trackedTabId, "goToTrack" + trackIdx);
+  chrome.tabs.sendMessage(trackedTabId, "goToTrack:" + trackIdx);
+}
+function goToPercent(percent) {
+  chrome.tabs.sendMessage(trackedTabId, "goToPercent:" + percent);
 }
 
 function refreshCurrentVideo(mainPopupLabel, secondaryPopupLabel, noTrackLabel, tracklistTable) {
